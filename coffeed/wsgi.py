09b806a65096b61a531c33b9d8fe4254d5fdf1e9
Alex Miles Younger <alex@alexmilesyounger.com>
2015-04-02 18:46:17 -0400

    Prepare for heroku

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -7,6 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+import os
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
