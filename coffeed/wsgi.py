24fcb8aa6a9a0a3437826290b55ed75f37658e48
Alex Miles Younger <alex@alexmilesyounger.com>
2015-04-02 18:55:32 -0400

    Update wsgi.py to try and launch on heroku

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -11,4 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
 application = get_wsgi_application()
