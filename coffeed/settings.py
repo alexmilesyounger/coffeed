1e55f99394f7c91f361ee86f735b3a70f11f6d79
Alex Miles Younger <alex@alexmilesyounger.com>
2015-04-02 17:56:13 -0400

    Add settings changes for Heroku and Procfile

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'coffeed.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(MAIN_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,9 +81,20 @@ WSGI_APPLICATION = 'coffeed.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
     }
 }
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 
 
 # Internationalization
@@ -103,10 +116,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
- 
+TEMPLATE_DIR = (
+    os.path.join(MAIN_DIR, 'templates'),
+    )
 
 STATICFILES_DIRS = (
- os.path.join(BASE_DIR, 'static'),
- )
+    os.path.join(MAIN_DIR, 'static'),
+    )
 
 STATIC_ROOT = 'staticfiles'
+
+
+
+
