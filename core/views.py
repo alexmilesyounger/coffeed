470dab3eed0a64eaac7c65d1f0ce9e5e80e8f7d2
Alex Miles Younger <alex@alexmilesyounger.com>
2015-04-03 10:55:59 -0400

    Update views for IndentationError

--- core/views.py
+++ core/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 # Create your views here. 
 
-
+class LandingView(TemplateView):
 	template_name = 'base/index.html'
