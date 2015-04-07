208e35aec23f010d9c5c3104dc2f05bf466615bd
Alex Miles Younger <alex@alexmilesyounger.com>
2015-04-07 19:51:48 -0400

    Add images to location list

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -138,7 +138,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWSSecretKey')
 AWS_ACCESS_KEY_ID = os.environ.get('AWSAccessKeyId')
 
 
-# WORKS
+# OLD WORKS
 # AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 # AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 
