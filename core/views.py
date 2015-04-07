46f0b850adf093b16a102a0d6b386e7df7701f13
Alex Miles Younger <alex@alexmilesyounger.com>
2015-04-07 15:37:33 -0400

    Create Post Detail View

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 import core.models as coremodels
 
 # Create your views here. 
@@ -12,7 +13,11 @@ class LandingView(TemplateView):
 class LocationListView(ListView):
 	model = coremodels.Location
 	template_name = 'location/list.html'
-	# template_name = 'base/theme.html'
+
+class LocationDetailView(DetailView):
+	model = coremodels.Location
+	template_name = 'location/detail.html'
+	context_object_name = 'location'
 
 # class LocationDetailView(DetailView):
 # 	model = coremodels.location
