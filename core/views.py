8c3024488d099f75d30b72f13c120c7f1037de43
Alex Miles Younger <alex@alexmilesyounger.com>
2015-04-07 12:35:18 -0400

    Creating a theme file + ERROR
    ERROR: The css/js doesn't seem to be coming through for the nav bar
    not sure what the solution is, but I'm moving on with it broken for
    the moment.

--- core/views.py
+++ core/views.py
@@ -11,4 +11,10 @@ class LandingView(TemplateView):
 
 class LocationListView(ListView):
 	model = coremodels.Location
-	template_name = 'location/list.html'
+	# template_name = 'location/list.html'
+	template_name = 'base/theme.html'
+
+# class LocationDetailView(DetailView):
+# 	model = coremodels.location
+# 	template_name = 'location/detail.html'
+# 	context_object_name = 'location'
